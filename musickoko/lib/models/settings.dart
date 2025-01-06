755a54f994e5a27324c6f6bd875ee4f0439216f8
Dukeazeta <azetaduke@gmail.com>
2025-01-06 18:28:00 +0100

    modified:   musickoko/lib/models/settings.dart

--- musickoko/lib/models/settings.dart
+++ musickoko/lib/models/settings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../services/storage_service.dart';
 
 class PlayerSettings {
   final bool enableGaplessPlayback;
