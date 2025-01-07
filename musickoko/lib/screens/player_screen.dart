99910e2cee5ef3ddb205e5fdbebee32af755f428
Dukeazeta <azetaduke@gmail.com>
2025-01-07 18:03:09 +0100

    modified:   musickoko/lib/screens/player_screen.dart

--- musickoko/lib/screens/player_screen.dart
+++ musickoko/lib/screens/player_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:musickoko/main.dart';
 import '../widgets/media_controls.dart';
 import '../widgets/music_card.dart';
 import '../widgets/audio_visualizer.dart';
